d9a98e748e6900389a2578aa46e67c14d8dbf8ba
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-02-09 19:50:53 +0800

    Improve performance by fewer lookups

--- src/onyx/peer/function.clj
+++ src/onyx/peer/function.clj
@@ -17,14 +17,13 @@
    {:onyx.core/batch (onyx.extensions/receive-messages messenger event)}))
 
 (defn write-batch
-  ([{:keys [onyx.core/results onyx.core/messenger onyx.core/state
-            onyx.core/replica onyx.core/peer-replica-view onyx.core/serialized-task] :as event}]
-   (write-batch event replica peer-replica-view state messenger (:egress-ids serialized-task)))
-  ([event replica peer-replica-view state messenger egress-tasks]
+  ([{:keys [onyx.core/messenger onyx.core/state
+            onyx.core/peer-replica-view onyx.core/serialized-task] :as event}]
+   (write-batch event peer-replica-view state messenger (:egress-ids serialized-task)))
+  ([event peer-replica-view state messenger egress-tasks]
    (let [segments (:segments (:onyx.core/results event))]
      (when-not (empty? segments)
-       (let [replica-val @replica
-             pick-peer-fns (:pick-peer-fns @peer-replica-view)
+       (let [pick-peer-fns (:pick-peer-fns @peer-replica-view)
              grouped (group-by #(t/vector (:route %) (:hash-group %)) segments)]
          (run! (fn [[[route hash-group] segs]]
                  (when-let [pick-peer-fn (get pick-peer-fns (get egress-tasks route))]
@@ -34,7 +33,7 @@
                grouped))))
    {}))
 
-(defrecord Function [replica peer-replica-view state messenger egress-tasks]
+(defrecord Function [peer-replica-view state messenger egress-tasks]
 
   p-ext/PipelineInput
   p-ext/Pipeline
@@ -45,18 +44,16 @@
 
   (write-batch
     [_ event]
-    (write-batch event replica peer-replica-view state messenger egress-tasks))
+    (write-batch event peer-replica-view state messenger egress-tasks))
 
   (seal-resource [_ _]
     nil))
 
-(defn function [{:keys [onyx.core/replica
-                        onyx.core/peer-replica-view
+(defn function [{:keys [onyx.core/peer-replica-view
                         onyx.core/state
                         onyx.core/messenger
                         onyx.core/serialized-task] :as pipeline-data}]
-  (->Function replica
-              peer-replica-view
+  (->Function peer-replica-view
               state
               messenger
               (:egress-ids serialized-task)))
